770cf6ed9f33002f9ea946457bd54bb5cf3ef7ad
Anna KGT <106539309+AnnaKGT@users.noreply.github.com>
2025-02-11 11:54:22 +0200

    Update currency-converter.py

--- currency-converter.py
+++ currency-converter.py
@@ -1,14 +1,14 @@
 '''
 https://www.currencyconverterapi.com/docs
-https://free.currconv.com/api/v8/currencies?apiKey=a923a45de7346b8b9504
-https://free.currconv.com/api/v8/currencies?apiKey=a923a45de7346b8b9504
+https://free.currconv.com/api/v8/currencies?apiKey=
+https://free.currconv.com/api/v8/currencies?apiKey=
 '''
 
 from requests import get
 from pprint import PrettyPrinter
 
 BASE_URL = "https://free.currconv.com"
-API_KEY = "a923a45de7346b8b9504"
+API_KEY = "___"
 
 printer = PrettyPrinter()
 
